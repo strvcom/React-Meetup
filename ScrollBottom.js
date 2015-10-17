f868391df2cda4fed5ee80249e58e9409e46e0f3
Daniel Kijkov <daniel.kijkov@strv.com>
2015-10-17 08:39:25 +0200

    ScrollBottom.js

---
+++ ScrollBottom.js
@@ -0,0 +1,27 @@
+componentDidUpdate() {
+	setTimeout(()=>{
+		this.scrollBottom();
+	},200);
+},
+
+scrollBottom() {
+	this.refs.helperViewInner.measure((oxInner, oyInner, widthInner, heightInner) => {
+		this.refs.container.measure((ox, oy, width, height) => {
+			if (heightInner < height) return;
+			this.refs.helperView.scrollTo(heightInner - height + oyInner);
+		});
+	});
+},
+
+render() {
+	return(
+		<View ref="container" style={styles.container}>
+			<ScrollView ref="helperView">
+				<View ref="helperViewInner" style={[styles.container, {paddingBottom: 60}]}>
+					<Messages data={this.state.data} />
+				</View>
+			</ScrollView>
+			<Form ref="form" for="message" add={this.addMessage} />
+		</View>
+	);
+}
