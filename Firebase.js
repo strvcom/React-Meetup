732880ff2f8961de690be0c65945c9c5c1ff3279
Daniel Kijkov <daniel.kijkov@strv.com>
2015-10-17 09:11:32 +0200

    Firebase.js

---
+++ Firebase.js
@@ -0,0 +1,16 @@
+var Firebase = require('firebase');
+var ref = new Firebase('https://rnchat.firebaseio.com/messages');
+
+componentDidMount() {
+	ref.once('value', () => this.setState({isLoading: false}))
+	this.childAdded = ref.on('child_added', (snapshot)=> {
+		var messages = this.state.data;
+		var message = snapshot.val();
+
+		messages.push(message);
+
+		this.setState({
+			data: messages
+		});
+	});
+},
