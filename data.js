63e2fb305f4097fb260f3edb920347ba9d5267f8
Daniel Kijkov <daniel.kijkov@strv.com>
2015-10-17 08:35:54 +0200

    data.js

---
+++ data.js
@@ -0,0 +1,27 @@
+var data = [
+	{
+		date: 1445063492153,
+		content: 'Hi! Ready for workshop?',
+		author: 'Daniel Kijkov'
+	},
+	{
+		date: 1445063535719,
+		content: 'I was born to be ready',
+		author: 'Vladimir Vanek'
+	},
+	{
+		date: 1445063558454,
+		content: 'Yo guys! Beer after workshop?',
+		author: 'Josef Zavisek'
+	},
+	{
+		date: 1445063565969,
+		content: 'I am in :)',
+		author: 'Vladimir Vanek'
+	},
+	{
+		date: 1445063571618,
+		content: 'Fo sho!',
+		author: 'Daniel Kijkov'
+	}
+]
