ff67e5a8c2d8f5eede178cf55d69adf1769e1d0c
Daniel Kijkov <daniel.kijkov@strv.com>
2015-11-02 14:03:01 +0100

    Form.js - components

--- Chat/app/Chat.js
+++ Chat/app/Chat.js
@@ -1,2 +1,33 @@
 // Chat.js
 // https://2cb30612.ngrok.com
+
+'use strict';
+
+var React = require('react-native');
+var {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+} = React;
+
+var Form = require('./components/Form');
+
+var Chat = React.createClass({
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Component</Text>
+        <Form />
+      </View>
+    );
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
+
+module.exports = Chat;
--- Chat/app/components/Form.js
+++ Chat/app/components/Form.js
@@ -1,2 +1,49 @@
 // Form.js
 // https://47c8d0b.ngrok.com
+
+'use strict';
+
+var React = require('react-native');
+var {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableHighlight,
+  View,
+} = React;
+
+var Form = React.createClass({
+  getInitialState(){
+    return {
+      text: ''
+    }
+  },
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          onChangeText={(text) => this.setState({text})}
+          value={this.state.text}
+        />
+        <TouchableHighlight
+          style={styles.button}
+          onPress={this._onPressButton}>
+          <Text style={styles.buttonText}>SEND</Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+});
+
+var styles = StyleSheet.create({
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1
+  }
+});
+
+module.exports = Form;
+
+
