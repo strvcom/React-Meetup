2dcc17793524a724f4710676ba5fcf012e491b7d
Daniel Kijkov <daniel.kijkov@strv.com>
2015-11-02 14:39:36 +0100

    Messages.js - styling

--- Chat/app/Chat.js
+++ Chat/app/Chat.js
@@ -14,6 +14,34 @@ var {
 var Form = require('./components/Form');
 var Messages = require('./components/Messages');
 
+var data = [
+  {
+    date: 1445063492153,
+    content: 'Hi! Ready for workshop?',
+    author: 'Daniel Kijkov'
+  },
+  {
+    date: 1445063535719,
+    content: 'I was born to be ready',
+    author: 'Vladimir Vanek'
+  },
+  {
+    date: 1445063558454,
+    content: 'Yo guys! Beer after workshop?',
+    author: 'Josef Zavisek'
+  },
+  {
+    date: 1445063565969,
+    content: 'I am in :)',
+    author: 'Vladimir Vanek'
+  },
+  {
+    date: 1445063571618,
+    content: 'Fo sho!',
+    author: 'Daniel Kijkov'
+  }
+]
+
 var Chat = React.createClass({
   addMessage(message) {
     console.log(message);
@@ -21,7 +49,7 @@ var Chat = React.createClass({
   render() {
     return (
       <View style={styles.container}>
-        <Messages />
+        <Messages data={data} />
         <Form add={this.addMessage} />
       </View>
     );
--- Chat/app/components/Messages.js
+++ Chat/app/components/Messages.js
@@ -6,27 +6,66 @@ var React = require('react-native');
 var {
   StyleSheet,
   Text,
-  TextInput,
-  TouchableHighlight,
+  ListView,
   View,
 } = React;
 
 var Messages = React.createClass({
   getInitialState(){
+    var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
     return {
-      text: ''
-    }
+      dataSource: ds,
+    };
+  },
+  renderRow(rowData) {
+    var {
+      date,
+      author,
+      content
+    } = rowData;
+
+    return (
+      <View style={styles.container}>
+        <View style={styles.row}>
+          <Text style={styles.author}>{author}</Text>
+          <Text style={styles.date}>{date}</Text>
+        </View>
+        <Text style={styles.content}>{content}</Text>
+      </View>
+    );
   },
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Messages</Text>
-      </View>
+      <ListView
+        dataSource={this.state.dataSource.cloneWithRows(this.props.data)}
+        renderRow={this.renderRow}
+      />
     );
   }
 });
 
 var styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    marginBottom: 5
+  },
+  container: {
+    padding: 10,
+    paddingHorizontal: 15,
+    borderColor: '#ddd',
+    borderBottomWidth: 1
+  },
+  author:{
+    fontWeight: 'bold',
+    color: '#c81b55',
+    flex: 1
+  },
+  date: {
+    color: '#666'
+  },
+  content: {
+
+  }
 });
 
 module.exports = Messages;
