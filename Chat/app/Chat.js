52dd85b7b48159871addfc26d27d38871537ab0a
Daniel Kijkov <daniel.kijkov@strv.com>
2015-11-02 10:26:06 +0100

    Messages.js

--- Chat/app/Chat.js
+++ Chat/app/Chat.js
@@ -10,6 +10,35 @@ var {
 } = React;
 
 var Form = require('./components/Form');
+var Messages = require('./components/Messages');
+
+var data = [
+  {
+    date: 1445063492153,
+    content: 'Hi! Ready for workshop?',
+    author: 'Daniel Kijkov'
+  },
+  {
+    date: 1445063535719,
+    content: 'I was born to be ready',
+    author: 'Vladimir Vanek'
+  },
+  {
+    date: 1445063558454,
+    content: 'Yo guys! Beer after workshop?',
+    author: 'Josef Zavisek'
+  },
+  {
+    date: 1445063565969,
+    content: 'I am in :)',
+    author: 'Vladimir Vanek'
+  },
+  {
+    date: 1445063571618,
+    content: 'Fo sho!',
+    author: 'Daniel Kijkov'
+  }
+]
 
 var Chat = React.createClass({
   addMessage(message){
@@ -17,7 +46,10 @@ var Chat = React.createClass({
   },
   render() {
     return(
-      <Form add={this.addMessage} />
+      <View>
+        <Messages data={data} />
+        <Form add={this.addMessage} />
+      </View>
     );
   }
 });
--- Chat/app/components/Messages.js
+++ Chat/app/components/Messages.js
@@ -1,1 +1,32 @@
 //messages.js
+
+var React = require('react-native');
+
+var {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableHighlight,
+  View,
+} = React;
+
+var Messages = React.createClass({
+  getInitialState() {
+    return {
+      text: ''
+    }
+  },
+  render() {
+    console.log(this.props.data);
+    return(
+      <View style={styles.container}>
+      </View>
+    );
+  }
+});
+
+var styles = StyleSheet.create({
+});
+
+module.exports = Messages;
