6eb6f5246c9f52d9e1f9bb994121092b5dddc95e
Daniel Kijkov <daniel.kijkov@strv.com>
2015-11-02 09:52:39 +0100

    init

---
+++ Chat/index.android.js
@@ -0,0 +1,17 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+var {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+} = React;
+
+var Chat = require('./app/Chat');
+
+AppRegistry.registerComponent('Chat', () => Chat);
